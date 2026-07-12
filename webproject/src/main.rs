511096d06d120585b940170780a55586cfb82802
MaudW <150226129+KNineW@users.noreply.github.com>
2026-07-12 15:53:56 +0800

    upload

---
+++ webproject/src/main.rs
@@ -0,0 +1,49 @@
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::fs;
+use std::io::prelude::*;
+use std::time::Duration;
+use std::thread;
+use webtcp::ThreadPool;
+fn main(){
+    let listener=TcpListener::bind("127.0.0.1:7878").unwrap();//监听
+    let pool=ThreadPool::new(4);
+    
+    for stream in listener.incoming().take(2){ //产生迭代器
+        let stream=stream.unwrap();
+        pool.execute(||{
+            handle_connection(stream);
+        });
+        //handle_connection(stream);
+        println!("Connection established!");
+    }
+    println!("Shutting down.")
+}
+
+fn handle_connection(mut stream:TcpStream){
+    let mut buffer=[0;1024];
+
+    stream.read(&mut buffer).unwrap();
+    //请求
+    //Method Request-URI HTTP-Version CRLF
+    //headers CRLF
+    //message-body
+    //响应
+    //HTTP_VERSION Status-Code Reason-Phrase CRLF
+    //header CRLF
+    //message-body
+    let get=b"GET /HTTP/1.1\r\n";//将get里的文本转换为字节字符串
+    let sleep=b"GET /sleep HTTP/1.1\r\n";
+    let (status_line,filename)=if buffer.starts_with(get){
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+    }else if buffer.starts_with(sleep){
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+    }else{
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
+    };
+    let contents=fs::read_to_string(filename).unwrap();
+    let response=format!("{}{}",status_line,contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+    
